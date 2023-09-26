8e40e14b23ca93622ed332637dc99d590f18f5da
hisham rehab <128677920+hishamrehab@users.noreply.github.com>
2023-09-26 11:26:28 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaGithubAlt } from "react-icons/fa";
+import Reviews from "./Reviews";
+function App() {
+  return (
+    <>
+      <main>
+        <section className="container">
+          <div className="title">
+            <h2>our reviews</h2>
+            <div className="underline"></div>
+          </div>
+          <Reviews/>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
